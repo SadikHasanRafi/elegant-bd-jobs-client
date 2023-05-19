e8793b674cd365e46404bb5adcb05feb2b560b60
NusratJahann <dilerakhatun52@gmail.com>
2023-05-19 17:24:41 +0600

    navbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,20 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  daisyui:{
+      themes:[
+      {
+        hiretoolTheme:{
+          primary: "#26bbac",
+          neutral: "#00e5bd",       
+          "base-100": "#FFFFFF",
+        },  
+      }
+    ],
+  },
   theme: {
-    extend: {},
+    extend: {
+    },
   },
   plugins: [require('daisyui')],
 }
