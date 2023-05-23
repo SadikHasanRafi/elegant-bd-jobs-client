10a4268bebac19f4345a0d7706e8d896dbd78399
SadikHasanRafi <sadikkhasan@gmail.com>
2023-05-23 19:18:36 +0600

    sign up email pass and google added

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,34 +1,62 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import "react";
 import auth from "../../firebase/firebase.config";
-
+import { AuthUserRoleContext } from "../../Contexts/authUserRoleContext";
+import { useContext } from "react";
 
 const SignUp = () => {
   const provider = new GoogleAuthProvider();
+  const {setUser,user} = useContext(AuthUserRoleContext)
+
+  const handleSignInByEmailPassword = async (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      setUser(userCredential.user)
+    })
+    .catch((error) => {
+      const errorMessage = error.message;
+      console.warn(errorMessage)
+    });
+
+    console.log(user)
+
+  };
 
   const handleSignInByGoogle = async () => {
     signInWithPopup(auth, provider)
-  .then((result) => {
-    const user = result.user;
-    console.log(user)
-  }).catch((error) => {
-    console.log(error)
-    // Handle Errors here.
-    // const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorMessage)
-    // The email of the user's account used.
-    const email = error.customData.email;
-    console.log(email)
-    // The AuthCredential type that was used.
-    // const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+        // Handle Errors here.
+        // const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage);
+        // The email of the user's account used.
+        const email = error.customData.email;
+        console.log(email);
+        // The AuthCredential type that was used.
+        // const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
 
     console.log("mew");
   };
   return (
     <div className="h-screen w-screen grid justify-items-center  place-content-center">
+      
+      <form onSubmit={handleSignInByEmailPassword}>
+        <input name="email" type="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter email" />
+        <input name="password" type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter password" />
+        <button type="submit" className="relative  inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white "><span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">Sign In</span></button>    
+      </form>
+
       <button
         type="button"
         className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br  shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
