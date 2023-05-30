adb29efe66f2290eea5342610530b6da42b3f8ec
SadikHasanRafi <sadikkhasan@gmail.com>
2023-05-30 21:39:49 +0600

    signup page ui done

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -72,22 +72,63 @@ const SignUp = () => {
     console.log("mew");
   };
   return (
-    <div className="h-screen w-screen grid justify-items-center  place-content-center">
+    <div >
       
-      <form onSubmit={handleSignInByEmailPassword}>
-        <input name="email" type="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter email" />
-        <input name="password" type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter password" />
-        <button type="submit" className="relative  inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white "><span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">Sign In</span></button>    
-      </form>
 
-      <button
-        type="button"
-        className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br  shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-        onClick={handleSignInByGoogle}
-      >
-        Signin with Google
-      </button>
-    </div>
+
+<div className="flex justify-center items-center min-h-screen">
+<div className="card w-96 border-[#e9e9e9] border-[0.2px] bg-base-100">
+<div className="card flex-shrink-0 w-full max-w-sm bg-base-100">
+<div className="card-body">
+<p className="text-center md:text-4xl text-3xl font-semibold text-primary mb-5">Sign up</p>
+
+<form onSubmit={handleSignInByEmailPassword}>
+<div className="form-control">
+<label className="label">
+  <span className="label-text">Email</span>
+</label>
+<input
+  type="text"
+  placeholder="email"
+  className="input input-bordered"
+/>
+</div>
+<div className="form-control">
+<label className="label">
+  <span className="label-text">Password</span>
+</label>
+<input
+  type="text"
+  placeholder="password"
+  className="input input-bordered"
+/>
+<label className="label">
+  <a href="#" className="label-text-alt link link-hover">
+    Forgot password?
+  </a>
+</label>
+</div>
+<div className="form-control mt-6">
+<button className="btn-style">Sign up</button>
+</div>
+
+
+</form>
+
+<div className="divider">OR</div>
+<div className="form-control">
+<button onClick={handleSignInByGoogle} className="btn-stroked">Sign up with Google</button>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+
+   
   );
 };
 
